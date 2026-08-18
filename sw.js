68eab6a75a5ab5bf9d759677e11d3fce44083cab
en78 <enaidu42@tntech.edu>
2026-08-17 21:41:48 -0400

    fixes

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Minimal Service Worker to satisfy PWA installability criteria
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
